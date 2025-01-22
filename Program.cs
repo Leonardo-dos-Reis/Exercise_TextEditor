a94f069563ab28156c9e09900c0baa0ae2658455
Leonardo dos Reis <leo.reis1802@hotmail.com>
2025-01-21 23:25:39 -0300

    Menu creation

--- Program.cs
+++ Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace TextEditor
+    {
+        class Program 
+            {
+                static void Main(string [] args)
+                    {
+                        Menu();
+                    }
+                
+                static void Menu()
+                    {
+
+                    Console.Clear();
+                    Console.WriteLine("Qual opção você deseja?");
+                    Console.WriteLine("");
+                    Console.WriteLine("1 - Abrir um arquivo");
+                    Console.WriteLine("2 - Criar um novo arquivo");
+                    Console.WriteLine("0 - Sair do programa");
+
+                    short opcao = short.Parse(Console.ReadLine());
+
+                    switch (opcao) {
+                        case 0: System.Environment.Exit(0); break;
+                    }
+
+                    }
+        }
+    }
